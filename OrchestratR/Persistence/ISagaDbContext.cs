3721ef19fa3bb22f1d1f5e22a69b1133a690a152
mixeilimisiko <miash20@freeuni.edu.ge>
2025-04-20 15:28:55 +0400

    feat: add SagaDbContext

---
+++ OrchestratR/Persistence/ISagaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrchestratR.Core;
+
+
+namespace OrchestratR.Persistence
+{
+    public interface ISagaDbContext
+    {
+        public DbSet<SagaEntity> Sagas { get; set; }
+    }
+}
---
+++ OrchestratR/Persistence/SagaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrchestratR.Core;
+
+namespace OrchestratR.Persistence
+{
+    public class SagaDbContext : DbContext
+    {
+        public DbSet<SagaEntity> Sagas { get; set; } = null!;
+        public SagaDbContext(DbContextOptions<SagaDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SagaEntity>()
+                .HasKey(e => e.SagaId);
+            // configure the ContextData to be stored as JSON in a column if using a database that supports JSON.
+            // for simplicity, take a text column.
+            modelBuilder.Entity<SagaEntity>()
+                .Property(e => e.Status).HasConversion<string>();
+            // Storing the enum as string for readability, might change to int for performance later
+        }
+    }
+}
