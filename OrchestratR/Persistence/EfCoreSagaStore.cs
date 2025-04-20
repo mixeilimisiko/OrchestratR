86a487a8dd1320559adff5c699c57357620c70f3
mixeilimisiko <miash20@freeuni.edu.ge>
2025-04-20 15:33:26 +0400

    feat: add EfCoreSagaStore stub

---
+++ OrchestratR/Persistence/EfCoreSagaStore.cs
@@ -0,0 +1,38 @@
+﻿using OrchestratR.Core;
+
+namespace OrchestratR.Persistence
+{
+
+    /// <summary>
+    /// Saga store backed by Entity Framework Core for persistent storage in a database.
+    /// </summary>
+    public class EfCoreSagaStore : ISagaStore
+    {
+        private readonly SagaDbContext _db;
+
+        public EfCoreSagaStore(SagaDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public Task<SagaEntity?> FindByIdAsync(Guid sagaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<SagaEntity>> FindByStatusAsync(SagaStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(SagaEntity saga)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(SagaEntity saga)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
