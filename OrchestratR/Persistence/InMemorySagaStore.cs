931fbffb98646aa50c87a292f19f53bb8eb92682
mixeilimisiko <miash20@freeuni.edu.ge>
2025-04-20 14:54:48 +0400

    feat: add InMemorySagaStore stub

---
+++ OrchestratR/Persistence/InMemorySagaStore.cs
@@ -0,0 +1,30 @@
+﻿
+using OrchestratR.Core;
+
+namespace OrchestratR.Persistence
+{
+    public class InMemorySagaStore : ISagaStore
+    {
+
+        public Task SaveAsync(SagaEntity saga)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(SagaEntity saga)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<SagaEntity?> FindByIdAsync(Guid sagaId)
+        {
+            return Task.FromResult<SagaEntity?>(null);
+        }
+
+        public Task<List<SagaEntity>> FindByStatusAsync(SagaStatus status)
+        {
+            return Task.FromResult(new List<SagaEntity>());
+        }
+
+    }
+}
