1c946b02eebfe6ff8c0cfcf4e0f786fe834be506
mixeilimisiko <miash20@freeuni.edu.ge>
2025-05-09 15:05:52 +0400

    feat: add StepBuilder

---
+++ OrchestratR/Builder/StepBuilder.cs
@@ -0,0 +1,24 @@
+﻿
+using OrchestratR.Core;
+using OrchestratR.Orchestration;
+
+namespace OrchestratR.Builder
+{
+    public class StepBuilder<TContext, TStep> where TStep : ISagaStep<TContext> where TContext : SagaContext
+    {
+        public Type StepType { get; } = default!;
+        private readonly SagaStepDefinition<TContext> _stepDef;
+        internal StepBuilder(SagaStepDefinition<TContext> stepDef) { _stepDef = stepDef; }
+
+        public StepBuilder<TContext, TStep> WithRetry(int maxRetries)
+        {
+            _stepDef.MaxRetries = maxRetries;
+            return this;
+        }
+        public StepBuilder<TContext, TStep> WithTimeout(TimeSpan timeout)
+        {
+            _stepDef.Timeout = timeout;
+            return this;
+        }
+    }
+}
