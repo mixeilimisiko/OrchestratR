02ed3290e5c188353d3df96f5919915582de6628
mixeilimisiko <miash20@freeuni.edu.ge>
2025-05-09 14:51:18 +0400

    feat: add SagaConfig and SagaStepDefinition for step metadata

---
+++ OrchestratR/Orchestration/SagaStepDefinition.cs
@@ -0,0 +1,16 @@
+﻿using OrchestratR.Core;
+
+namespace OrchestratR.Orchestration
+{
+    public class SagaStepDefinition<TContext> where TContext : SagaContext
+    {
+        public Type StepType { get; }
+        public int MaxRetries { get; set; }
+        public TimeSpan? Timeout { get; set; }
+
+        public SagaStepDefinition(Type stepType)
+        {
+            StepType = stepType;
+        }
+    }
+}
