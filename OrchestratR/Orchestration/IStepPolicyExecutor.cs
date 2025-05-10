f6dbde827b2d69b6e38621818656df082e76bdbe
mixeilimisiko <miash20@freeuni.edu.ge>
2025-05-10 09:10:10 +0400

    feat: introduce IStepPolicyExecutor interface

---
+++ OrchestratR/Orchestration/IStepPolicyExecutor.cs
@@ -0,0 +1,9 @@
+﻿using OrchestratR.Core;
+
+namespace OrchestratR.Orchestration
+{
+    public interface IStepPolicyExecutor<TContext>
+    {
+        Task<SagaStepStatus> ExecuteAsync(Func<Task<SagaStepStatus>> stepExecution);
+    }
+}
