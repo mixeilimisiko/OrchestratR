f7bddaa83f50ea1df2994a99fb18c62a792d7ed2
mixeilimisiko <miash20@freeuni.edu.ge>
2025-05-16 17:29:12 +0400

    feat: support resuming sagas with context patching via lambda'

--- OrchestratR/Registration/SagaRegistrationExtensions.cs
+++ OrchestratR/Registration/SagaRegistrationExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using OrchestratR.Core;
 using OrchestratR.Persistence;
 
@@ -43,51 +42,6 @@ namespace OrchestratR.Registration
             }
 
             return services;
-        }
-    }
-
-
-    public class SagaInfrastructureOptions
-    {
-        internal bool UseEfCoreEnabled { get; private set; }
-        internal bool UseInMemoryEnabled { get; private set; }
-        internal bool SkipMigrations { get; private set; }
-        internal Action<DbContextOptionsBuilder>? DbContextOptionsAction { get; private set; }
-
-        public static string GetMigrationsAssembly()
-        {
-            return typeof(SagaDbContext).Assembly.FullName ?? string.Empty;
-        }
-
-        // Consumer will call this to use EF Core as the saga store
-        public SagaInfrastructureOptions UseEfCore(Action<DbContextOptionsBuilder> optionsAction)
-        {
-            UseEfCoreEnabled = true;
-
-            DbContextOptionsAction = dbCtxOptions =>
-            {
-                // Let the user fully configure provider and connection
-                optionsAction(dbCtxOptions);
-
-                // log if MigrationsAssembly is missing
-                // users should configure it themselves. 
-
-            };
-
-            return this;
-        }
-        public SagaInfrastructureOptions UseInMemory()
-        {
-            UseInMemoryEnabled = true;
-
-            return this;
-        }
-
-        // Consumer can call this to disable auto-migration
-        public SagaInfrastructureOptions SkipMigrationApplication()
-        {
-            SkipMigrations = true;
-            return this;
         }
     }
 }
